a19a5a45fdbded05d627b49c010a63fea856117a
Ahmad Khairol Maulidi <a.khairol.m@gmail.com>
2023-10-09 21:24:25 +0800

    Update: Belajar Unit Testing

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,12 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { UsersService } from '../users/users.service';
+import { User } from 'src/users/user.entity';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
+    const fakeUsersService: Partial<UsersService> = {
+      find: () => Promise.resolve([]),
+      create: (name: string, email: string, password: string) => {
+        return Promise.resolve({ id: 1, name, email, password} as User);
+      },
+    }
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [AuthService, {
+        provide: UsersService,
+        useValue: fakeUsersService,
+      }],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
@@ -15,4 +27,15 @@ describe('AuthService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('should create a new user', async () => {
+    const user = await service.register('John Doe', 'awdjadjaj@example.com', 'password');
+
+    expect(user.password).not.toEqual('password');
+    const [salt, hash] = user.password.split('.');
+    console.log(user);
+    expect(salt).toBeDefined();
+    expect(hash).toBeDefined();
+  });
+
 });
