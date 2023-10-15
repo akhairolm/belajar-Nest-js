ea36627965c17bc1296eec9997ab71381834722f
Ahmad Khairol Maulidi <a.khairol.m@gmail.com>
2023-10-15 20:55:38 +0800

    Update: Setup Mock Unit Testing Auth Controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,41 +1,55 @@
-import { Body, Controller, Get, Post, Session, UseInterceptors } from '@nestjs/common';
-import { CreateUserDto } from 'src/users/dtos/create-user.dto';
-import { LoginUserDTO } from 'src/auth/dtos/login-user.dto';
-import { UsersService } from 'src/users/users.service';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Session,
+  UseInterceptors,
+} from '@nestjs/common';
+import { CreateUserDto } from '../users/dtos/create-user.dto';
+import { LoginUserDTO } from './dtos/login-user.dto';
+import { UsersService } from '../users/users.service';
 import { AuthService } from './auth.service';
-import { Serialize } from 'src/interceptors/serialize.interceptor';
-import { UserDto } from 'src/users/dtos/user.dto';
+import { Serialize } from '../interceptors/serialize.interceptor';
+import { UserDto } from '../users/dtos/user.dto';
 import { CurrentUser } from './decorators/current-user.decorator';
 import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
-import { User } from 'src/users/user.entity';
+import { User } from '../users/user.entity';
 
 @Controller('auth')
 @Serialize(UserDto)
 @UseInterceptors(CurrentUserInterceptor)
 export class AuthController {
-    constructor(private usersService: UsersService, private authService: AuthService) {}
+  constructor(
+    private usersService: UsersService,
+    private authService: AuthService,
+  ) {}
 
-    @Post('/register')
-    async register(@Body() body: CreateUserDto, @Session() session:any) {
-        const user = await this.authService.register(body.name, body.email, body.password);
-        session.userId = user.id;
-        return user;
-    }
+  @Post('/register')
+  async register(@Body() body: CreateUserDto, @Session() session: any) {
+    const user = await this.authService.register(
+      body.name,
+      body.email,
+      body.password,
+    );
+    session.userId = user.id;
+    return user;
+  }
 
-    @Post('/login')
-    async login(@Body() body: LoginUserDTO, @Session() session:any) {
-        const user = await this.authService.login(body.email, body.password);
-        session.userId = user.id;
-        return user;
-    }
+  @Post('/login')
+  async login(@Body() body: LoginUserDTO, @Session() session: any) {
+    const user = await this.authService.login(body.email, body.password);
+    session.userId = user.id;
+    return user;
+  }
 
-    @Post('/logout')
-    logout(@Session() session: any) {
-        session.userId = null;
-    }
+  @Post('/logout')
+  logout(@Session() session: any) {
+    session.userId = null;
+  }
 
-    @Get('/whoami')
-    whoAmI(@CurrentUser() user: User) {
-        return user;
-    }
+  @Get('/whoami')
+  whoAmI(@CurrentUser() user: User) {
+    return user;
+  }
 }
--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -5,20 +5,37 @@ import { User } from 'src/users/user.entity';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let fakeUsersService: Partial<UsersService>;
 
   beforeEach(async () => {
-    const fakeUsersService: Partial<UsersService> = {
-      find: () => Promise.resolve([]),
-      create: (name: string, email: string, password: string) => {
-        return Promise.resolve({ id: 1, name, email, password} as User);
+    const users: User[] = [];
+
+    fakeUsersService = {
+      find: (email: string) => {
+        const user = users.filter((user) => user.email === email);
+        return Promise.resolve(user);
       },
-    }
+      create: (name: string, email: string, password: string) => {
+        const user = {
+          id: Math.floor(Math.random() * 999999),
+          name,
+          email,
+          password,
+        } as User;
+
+        users.push(user);
+        return Promise.resolve(user);
+      },
+    };
 
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService, {
-        provide: UsersService,
-        useValue: fakeUsersService,
-      }],
+      providers: [
+        AuthService,
+        {
+          provide: UsersService,
+          useValue: fakeUsersService,
+        },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
@@ -29,13 +46,43 @@ describe('AuthService', () => {
   });
 
   it('should create a new user', async () => {
-    const user = await service.register('John Doe', 'awdjadjaj@example.com', 'password');
+    const user = await service.register(
+      'John Doe',
+      'awdjadjaj@example.com',
+      'password',
+    );
 
+    expect(user.name).toBe('John Doe');
     expect(user.password).not.toEqual('password');
     const [salt, hash] = user.password.split('.');
-    console.log(user);
     expect(salt).toBeDefined();
     expect(hash).toBeDefined();
   });
 
+  it('should fail to create a user with an existing email', async () => {
+    await service.register('John Doe', 'awdjadjaj@example.com', 'password');
+    await expect(
+      service.register('John Doe', 'awdjadjaj@example.com', 'password'),
+    ).rejects.toThrow('Email sudah terdaftar');
+  });
+
+  it('throws if user login with invalid email', async () => {
+    await expect(service.login('admin@mail.com', 'password')).rejects.toThrow(
+      'Email tidak terdaftar',
+    );
+  });
+
+  it('should fail if user login with invalid password', async () => {
+    await service.register('John Doe', 'awdjadjaj@example.com', 'test123');
+
+    await expect(
+      service.login('awdjadjaj@example.com', 'password'),
+    ).rejects.toThrow('Password salah');
+  });
+
+  it('should login existing user', async () => {
+    await service.register('John Doe', 'awdjadjaj@example.com', 'password');
+    const user = await service.login('awdjadjaj@example.com', 'password');
+    expect(user).toBeDefined();
+  });
 });
