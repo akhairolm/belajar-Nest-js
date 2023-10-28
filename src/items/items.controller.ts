361312b2229686b276e2c88fba8206b1e21455f7
Ahmad Khairol Maulidi <a.khairol.m@gmail.com>
2023-10-28 22:10:50 +0800

    Menerapkan Validator Pada Item Dto

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { createItemDto } from './dto/create-item.dto';
+import { ItemsService } from './items.service';
 
 @Controller('items')
-export class ItemsController {}
+export class ItemsController {
+  constructor(private itemService: ItemsService) {}
+  @Post()
+  createItem(@Body() body: createItemDto) {
+    return this.itemService.create(body);
+  }
+}
